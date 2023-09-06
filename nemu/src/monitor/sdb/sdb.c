e36ac80857c4192e560f8520e6004ef0ff416594
Songmw <1397472492@qq.com>
2023-09-06 20:39:13 +0800

    bugfix:退出逻辑

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -189,8 +189,7 @@ void sdb_mainloop() {
         for (i = 0; i < NR_CMD; i++) {
             if (strcmp(cmd, cmd_table[i].name) == 0) {
                 if (cmd_table[i].handler(args) < 0) {
-                    printf("Command execution error!!! \n");
-                    //return;
+                    return;
                 }
                 break;
             }
